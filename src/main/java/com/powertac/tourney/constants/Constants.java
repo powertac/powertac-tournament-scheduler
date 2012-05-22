ba8fb469a6e93ad6750a0e28d91a1a47814edd12
Erik Onarheim <onarh001@umn.edu>
2012-05-22 17:02:28 -0500

    Forgot a database param

--- src/main/java/com/powertac/tourney/constants/Constants.java
+++ src/main/java/com/powertac/tourney/constants/Constants.java
@@ -322,7 +322,7 @@ public class Constants {
 	 * @param gameId : The gameId that this property file belongs to
 	 * @param tourneyId : The tournament the game belongs to (THis is denormalization to spead up queries)
 	 */
-	public static final String ADD_PROPERTIES = "INSERT INTO tourney.properties (jmsUrl,location,startTime,gameId) VALUES ('',?,?,?);";
+	public static final String ADD_PROPERTIES = "INSERT INTO tourney.properties (jmsUrl, vizQueue,location,startTime,gameId) VALUES ('','',?,?,?);";
 	
 	/***
 	 * Update the properties with jmsUrl for sims, this is done as soon as you know the machine you're scheduling on
