4137cfd10b733fd45f4ca462a6070d4f0d21efd9
Erik Onarheim <onarh001@umn.edu>
2012-05-23 00:41:19 -0500

    Added Competition bean

---
+++ src/main/java/com/powertac/tourney/beans/Competition.java
@@ -0,0 +1,27 @@
+package com.powertac.tourney.beans;
+
+public class Competition {
+	private String status;
+	private int qualifierRoundId;
+	private int finalRoundId;
+	
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getQualifierRoundId() {
+		return qualifierRoundId;
+	}
+	public void setQualifierRoundId(int qualifierRoundId) {
+		this.qualifierRoundId = qualifierRoundId;
+	}
+	public int getFinalRoundId() {
+		return finalRoundId;
+	}
+	public void setFinalRoundId(int finalRoundId) {
+		this.finalRoundId = finalRoundId;
+	}
+}
