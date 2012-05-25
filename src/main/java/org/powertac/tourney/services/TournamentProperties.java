c840c999f845b3623a6b9dff95208e546d9d627b
John Collins <jcollins@cs.umn.edu>
2012-05-24 21:29:22 -0500

    break out properties service

---
+++ src/main/java/org/powertac/tourney/services/TournamentProperties.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2012 by the original author
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.powertac.tourney.services;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Central source of Properties read from tournament.properties
+ * @author John Collins
+ */
+@Service("tournamentProperties")
+public class TournamentProperties
+{
+  private Properties props;
+  private boolean loaded = false;
+  private String resourceName = "/tournament.properties";
+  
+  // delegate to props
+  public String getProperty (String key)
+  {
+    loadIfNecessary();
+    return props.getProperty(key);
+  }
+  
+  public String getProperty (String key, String defaultValue)
+  {
+    loadIfNecessary();
+    return props.getProperty(key, defaultValue);
+  }
+  
+  // lazy loader
+  private void loadIfNecessary ()
+  {
+    if (!loaded) {
+      try {
+        props.load(TournamentProperties.class.getClassLoader()
+                   .getResourceAsStream(resourceName));
+        loaded = true;
+      }
+      catch (IOException e) {
+        System.out.println("[ERROR] Failed to load " + resourceName);
+      }
+    }
+  }
+}
