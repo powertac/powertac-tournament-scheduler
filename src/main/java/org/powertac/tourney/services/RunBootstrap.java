d92cb982efa30a5fc870b786391d83c22636b619
Erik Onarheim <onarh001@umn.edu>
2012-05-26 23:24:30 -0500

    Test

--- src/main/java/org/powertac/tourney/services/RunBootstrap.java
+++ src/main/java/org/powertac/tourney/services/RunBootstrap.java
@@ -11,16 +11,16 @@ import java.util.TimerTask;
 import org.powertac.tourney.beans.Machine;
 import org.springframework.stereotype.Service;
 
-
 public class RunBootstrap extends TimerTask
 {
 
   private String logSuffix = "boot-";// boot-game-" + game.getGameId() + "-tourney-"+
-                             // game.getCompetitionName();
+  // game.getCompetitionName();
   private String tourneyUrl = "";// game.getTournamentSchedulerUrl();
   private String serverConfig = "";// game.getServerConfigUrl();
-  private String bootstrapUrl = "";// This needs to be empty for jenkins to run a
-                           // bootstrapgame.getBootstrapUrl();
+  private String bootstrapUrl = "";// This needs to be empty for jenkins to run
+                                   // a
+  // bootstrapgame.getBootstrapUrl();
   private String pomUrl = "";// game.getPomUrl();
   private String gameId = "";// String.valueOf(game.getGameId());
   private String brokers = "";
@@ -39,7 +39,7 @@ public class RunBootstrap extends TimerTask
     this.serverConfig =
       tourneyUrl + "/faces/properties.jsp?gameId=" + this.gameId;
   }
-  
+
   public RunBootstrap (int gameId, String tourneyUrl, String pomUrl,
                        String destination, String machineName)
   {
@@ -56,52 +56,52 @@ public class RunBootstrap extends TimerTask
 
   private void checkMachineAvailable ()
   {
-   
-   
-    Database db = new Database();
-    try {
-      db.startTrans();
-      List<Machine> machines = db.getMachines();
-      List<Machine> available = new ArrayList<Machine>();
-      for (Machine m: machines) {
-        if (m.getStatus().equalsIgnoreCase("idle") && m.isAvailable()) {
-          available.add(m);
+
+    if (machineName.equalsIgnoreCase("")) {
+      Database db = new Database();
+      try {
+        db.startTrans();
+        List<Machine> machines = db.getMachines();
+        List<Machine> available = new ArrayList<Machine>();
+        for (Machine m: machines) {
+          if (m.getStatus().equalsIgnoreCase("idle") && m.isAvailable()) {
+            available.add(m);
+          }
+        }
+        if (available.size() > 0) {
+
+          // db.updateGameJmsUrlById(Integer.parseInt(gameId),"tcp://"+
+          // available.get(0).getName() +":61616");
+          db.updateGameMachine(Integer.parseInt(gameId), available.get(0)
+                  .getMachineId());
+          db.setMachineStatus(available.get(0).getMachineId(), "running");
+          this.machineName = available.get(0).getName();
+          System.out.println("Running boot " + gameId + " on machine "
+                             + this.machineName);
+          db.commitTrans();
+        }
+        else {
+          db.abortTrans();
+          System.out.println("No machines available to run scheduled boot: "
+                             + gameId + " ... will retry in 5 minutes");
+          // Thread.sleep(300000);
+          // this.run();
         }
       }
-      if (available.size() > 0) {
-
-        // db.updateGameJmsUrlById(Integer.parseInt(gameId),"tcp://"+
-        // available.get(0).getName() +":61616");
-        db.updateGameMachine(Integer.parseInt(gameId), available.get(0)
-                .getMachineId());
-        db.setMachineStatus(available.get(0).getMachineId(), "running");
-        this.machineName = available.get(0).getName();
-        System.out.println("Running boot " + gameId + " on machine "
-                           + this.machineName);
-        db.commitTrans();
+      catch (NumberFormatException e) {
+        e.printStackTrace();
       }
-      else {
-        db.abortTrans();
-        System.out.println("No machines available to run scheduled boot: "
-                           + gameId + " ... will retry in 5 minutes");
-        //Thread.sleep(300000);
-        //this.run();
+      catch (SQLException e) {
+        e.printStackTrace();
       }
-    }
-    catch (NumberFormatException e) {
-      e.printStackTrace();
-    }
-    catch (SQLException e) {
-      e.printStackTrace();
     }
 
   }
 
   public void run ()
   {
-    if(machineName == null){
-      checkMachineAvailable();
-    }
+
+    checkMachineAvailable();
 
     String finalUrl =
       "http://localhost:8080/jenkins/job/"
