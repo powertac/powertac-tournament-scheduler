0ac102e81ef2ee5a85a24885895cc029cd8bde30
Erik Onarheim <onarh001@umn.edu>
2012-05-31 04:59:49 -0500

    Fix query

--- src/main/java/org/powertac/tourney/beans/Scheduler.java
+++ src/main/java/org/powertac/tourney/beans/Scheduler.java
@@ -233,7 +233,7 @@ public class Scheduler
       System.out.println("[INFO] Freeing agents on " + serverNumber);
       String freeAgents = "%s";
       String.format(freeAgents, Constants.FREE_AGENTS_ON_SERVER);
-      freeAgents.replace("\\?", String.valueOf(serverNumber));
+      freeAgents.replace("?", String.valueOf(serverNumber));
       System.out.println("[INFO] Query: " + freeAgents);
       db.SetQuery(freeAgents, "update");
     }
