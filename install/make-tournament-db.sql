47d8281644f7b35565676aba11257640f6990aaa
Govert Buijs <buijs@rsm.nl>
2019-01-07 09:36:21 +0100

    Security update

--- install/make-tournament-db.sql
+++ install/make-tournament-db.sql
@@ -1,15 +1,15 @@
 DROP DATABASE IF EXISTS `powertac_tournament`;
-CREATE DATABASE `powertac_tournament`;
+CREATE DATABASE `powertac_tournament` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 /* Create user table*/
 DROP TABLE IF EXISTS `powertac_tournament`.`users`;
 CREATE TABLE `powertac_tournament`.`users` (
   `userId`       INT(11)                                                     NOT NULL AUTO_INCREMENT,
   `userName`     VARCHAR(45) UNIQUE                                          NOT NULL,
-  `institution`  VARCHAR(256)                                                NULL,
-  `contactName`  VARCHAR(256)                                                NULL,
-  `contactEmail` VARCHAR(256)                                                NULL,
-  `contactPhone` VARCHAR(256)                                                NULL,
+  `institution`  VARCHAR(255)                                                NULL,
+  `contactName`  VARCHAR(255)                                                NULL,
+  `contactEmail` VARCHAR(255)                                                NULL,
+  `contactPhone` VARCHAR(255)                                                NULL,
   `salt`         VARCHAR(45)                                                 NOT NULL,
   `password`     VARCHAR(45)                                                 NOT NULL,
   `permission`   ENUM('admin', 'researcher', 'organizer', 'broker', 'guest') NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE `powertac_tournament`.`brokers` (
   `userId`      INT(11)             NOT NULL,
   `brokerName`  VARCHAR(45) UNIQUE  NOT NULL,
   `brokerAuth`  VARCHAR(32) UNIQUE  NOT NULL,
-  `brokerShort` VARCHAR(256)        NOT NULL,
+  `brokerShort` VARCHAR(255)        NOT NULL,
   PRIMARY KEY (`brokerId`),
   CONSTRAINT broker_refs FOREIGN KEY (`userId`) REFERENCES `powertac_tournament`.`users` (`userId`)
 )
@@ -45,7 +45,7 @@ CREATE TABLE `powertac_tournament`.`poms` (
 DROP TABLE IF EXISTS `powertac_tournament`.`tournaments`;
 CREATE TABLE `powertac_tournament`.`tournaments` (
   `tournamentId`   INT(11)                                                                                                                                            NOT NULL AUTO_INCREMENT,
-  `tournamentName` VARCHAR(256) UNIQUE                                                                                                                                NOT NULL,
+  `tournamentName` VARCHAR(255) UNIQUE                                                                                                                                NOT NULL,
   `state`          ENUM('open', 'closed', 'scheduled0', 'completed0', 'scheduled1', 'completed1', 'scheduled2', 'completed2', 'scheduled3', 'completed3', 'complete') NOT NULL,
   `pomId`          INT(11)                                                                                                                                            NOT NULL,
   `maxAgents`      INT(11)                                                                                                                                            NOT NULL DEFAULT 2,
@@ -58,7 +58,7 @@ CREATE TABLE `powertac_tournament`.`tournaments` (
 DROP TABLE IF EXISTS `powertac_tournament`.`levels`;
 CREATE TABLE `powertac_tournament`.`levels` (
   `levelId`      INT(11)      NOT NULL AUTO_INCREMENT,
-  `levelName`    VARCHAR(256) NOT NULL,
+  `levelName`    VARCHAR(255) NOT NULL,
   `tournamentId` INT(11)      NOT NULL,
   `levelNr`      INT(11)      NOT NULL,
   `nofRounds`    INT(11)      NOT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE `powertac_tournament`.`levels` (
 DROP TABLE IF EXISTS `powertac_tournament`.`rounds`;
 CREATE TABLE `powertac_tournament`.`rounds` (
   `roundId`     INT(11)                                    NOT NULL AUTO_INCREMENT,
-  `roundName`   VARCHAR(256) UNIQUE                        NOT NULL,
+  `roundName`   VARCHAR(255) UNIQUE                        NOT NULL,
   `levelId`     INT(11) DEFAULT NULL,
   `startTime`   DATETIME                                   NOT NULL,
   `dateFrom`    DATETIME                                   NOT NULL,
@@ -87,7 +87,7 @@ CREATE TABLE `powertac_tournament`.`rounds` (
   `multiplier1` INT(11)                                    NOT NULL,
   `multiplier2` INT(11)                                    NOT NULL,
   `multiplier3` INT(11)                                    NOT NULL,
-  `locations`   VARCHAR(256)                               NOT NULL,
+  `locations`   VARCHAR(255)                               NOT NULL,
   `state`       ENUM('pending', 'in_progress', 'complete') NOT NULL,
   PRIMARY KEY (`roundId`),
   CONSTRAINT round_refs1 FOREIGN KEY (`levelId`) REFERENCES `powertac_tournament`.`levels` (`levelId`)
@@ -123,8 +123,8 @@ DROP TABLE IF EXISTS `powertac_tournament`.`machines`;
 CREATE TABLE `powertac_tournament`.`machines` (
   `machineId`     INT(11)                 NOT NULL AUTO_INCREMENT,
   `machineName`   VARCHAR(32)             NOT NULL,
-  `machineUrl`    VARCHAR(256)            NOT NULL, /* Hostname of the machine */
-  `visualizerUrl` VARCHAR(256)            NOT NULL,
+  `machineUrl`    VARCHAR(255)            NOT NULL, /* Hostname of the machine */
+  `visualizerUrl` VARCHAR(255)            NOT NULL,
   `state`         ENUM('idle', 'running') NOT NULL, /* Indicates wether a game is running on this machine or not, either "running" or "idle" */
   `available`     BOOLEAN                 NOT NULL,
   PRIMARY KEY (`machineId`)
@@ -136,16 +136,16 @@ CREATE TABLE `powertac_tournament`.`machines` (
 DROP TABLE IF EXISTS `powertac_tournament`.`games`;
 CREATE TABLE `powertac_tournament`.`games` (
   `gameId`          INT(11)                                                                                                                                                    NOT NULL AUTO_INCREMENT,
-  `gameName`        VARCHAR(256)                                                                                                                                               NOT NULL,
+  `gameName`        VARCHAR(255)                                                                                                                                               NOT NULL,
   `roundId`         INT(11)                                                                                                                                                    NOT NULL,
   `machineId`       INT(11)                                                                                                                                                    NULL,
   `state`           ENUM('boot_pending', 'boot_in_progress', 'boot_complete', 'boot_failed', 'game_pending', 'game_ready', 'game_in_progress', 'game_complete', 'game_failed') NOT NULL,
   `startTime`       DATETIME                                                                                                                                                   NOT NULL,
   `readyTime`       DATETIME                                                                                                                                                   NULL,
-  `visualizerQueue` VARCHAR(256)                                                                                                                                               NOT NULL, /* name of visualizer output queue */
-  `serverQueue`     VARCHAR(256)                                                                                                                                               NOT NULL, /* name of server input queue */
-  `location`        VARCHAR(256)                                                                                                                                               NOT NULL,
-  `simStartDate`    VARCHAR(256)                                                                                                                                               NOT NULL,
+  `visualizerQueue` VARCHAR(255)                                                                                                                                               NOT NULL, /* name of visualizer output queue */
+  `serverQueue`     VARCHAR(255)                                                                                                                                               NOT NULL, /* name of server input queue */
+  `location`        VARCHAR(255)                                                                                                                                               NOT NULL,
+  `simStartDate`    VARCHAR(255)                                                                                                                                               NOT NULL,
   `gameLength`      INT(11) DEFAULT NULL,
   `lastTick`        INT(11) DEFAULT NULL,
   PRIMARY KEY (`gameId`),
@@ -158,7 +158,7 @@ CREATE TABLE `powertac_tournament`.`games` (
 DROP TABLE IF EXISTS `powertac_tournament`.`locations`;
 CREATE TABLE `powertac_tournament`.`locations` (
   `locationId` INT(11)      NOT NULL AUTO_INCREMENT,
-  `location`   VARCHAR(256) NOT NULL,
+  `location`   VARCHAR(255) NOT NULL,
   `timezone`   INT(11)      NOT NULL,
   `fromDate`   DATETIME     NOT NULL,
   `toDate`     DATETIME     NOT NULL,
@@ -189,7 +189,7 @@ CREATE TABLE `powertac_tournament`.`agents` (
 DROP TABLE IF EXISTS `powertac_tournament`.`config`;
 CREATE TABLE IF NOT EXISTS `powertac_tournament`.`config` (
   `configId`    INT(11)             NOT NULL AUTO_INCREMENT,
-  `configKey`   VARCHAR(256) UNIQUE NOT NULL,
+  `configKey`   VARCHAR(255) UNIQUE NOT NULL,
   `configValue` LONGTEXT,
   PRIMARY KEY (`configId`)
 )
